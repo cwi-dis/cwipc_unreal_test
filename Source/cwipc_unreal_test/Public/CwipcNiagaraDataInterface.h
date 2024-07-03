b0a2ee40da37d8085d2a1782056bb21ab22191a2
VRTINY\DIS <cwidisgroup@gmail.com>
2024-07-03 15:08:34 +0200

    Added empty CwipcNiagaraDataInterface class

---
+++ Source/cwipc_unreal_test/Public/CwipcNiagaraDataInterface.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NiagaraDataInterface.h"
+#include "CwipcNiagaraDataInterface.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CWIPC_UNREAL_TEST_API UCwipcNiagaraDataInterface : public UNiagaraDataInterface
+{
+	GENERATED_BODY()
+	
+};
